261959365cfe28fcd64a90ab0b88e4ad51352e74
subin <subin.c.park@gmail.com>
2014-08-01 21:10:38 +0900

    bin 파일 정리

--- src/com/example/explorethetown/QuestionActivity.java
+++ src/com/example/explorethetown/QuestionActivity.java
@@ -1,23 +1,27 @@
 package com.example.explorethetown;
 
+
+import android.annotation.SuppressLint;
 import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class QuestionActivity extends ActionBarActivity{
+public class QuestionActivity extends ActionBarActivity implements OnClickListener{
 	int questionNumber;
-	ImageButton imgBtnYES;
-	ImageButton imgBtnNO;
-	ImageButton imgBtnNext;
+	ImageView img_question;
+	ImageView btn_yes;
+	ImageView btn_no;
+	ImageView btn_next;
 	
-	TextView questionText;
-	
-	Question question;
-	
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -32,63 +36,49 @@ public class QuestionActivity extends ActionBarActivity{
 		int qNum = 1;
 		
 		
-		question = new Question(qNum*10+1);
+		//question = new Question(qNum*10+1);
 
-		imgBtnYES = (ImageButton)findViewById(R.id.imgBtnQ_YES);
-		imgBtnNO = (ImageButton)findViewById(R.id.imgBtnQ_NO);
-		imgBtnNext = (ImageButton)findViewById(R.id.imgBtnQ_Next);
+		img_question = (ImageView)findViewById(R.id.d_question_health);
+		btn_yes = (ImageView)findViewById(R.id.d_question_health_yes);
+		btn_no = (ImageView)findViewById(R.id.d_question_health_no);
+		btn_next = (ImageView)findViewById(R.id.imgBtn_D_Next);
 		
-		questionText = (TextView)findViewById(R.id.questionText);
 		
 		// Set Image buttons click listener
-		setOnClickListener();
+		initEvents();
 		// set question view 
-		showQuestionView();
+		//showQuestionView();
+		
+		
+		//TODO 다음 문제로 넘어갈 때 새로 이미지파일을 불러줘야 하는데 몇번으로 넘어갔는지 keep track 하는 것이 필
+		
+		
+		
 	}
 	
-	
-	public void setOnClickListener(){
-		
-		imgBtnYES.setOnClickListener(new View.OnClickListener(){
-			@Override
-			public void onClick(View arg0){
-				if(question.isEndQuestion()){
-					Intent iIntent = new Intent(QuestionActivity.this, AnswerMapActivity.class);
-					startActivity(iIntent);
-				}else{
-					question.nextQuestion();
-					showQuestionView();
-				}
-			}
-		});
-		
-		imgBtnNO.setOnClickListener(new View.OnClickListener(){
-			@Override
-			public void onClick(View arg0){
-				if(question.isEndQuestion()){
-					Intent iIntent = new Intent(QuestionActivity.this, AnswerMapActivity.class);
-					startActivity(iIntent);
-					
-				}else{
-					question.nextQuestion();
-					showQuestionView();
-				}
-			}
-		});
-		imgBtnNext.setOnClickListener(new View.OnClickListener(){
-			@Override
-			public void onClick(View arg0){
-				if(question.isEndQuestion()){
-					Intent iIntent = new Intent(QuestionActivity.this, AnswerMapActivity.class);
-					startActivity(iIntent);
-				}else{
-					question.nextQuestion();
-					showQuestionView();
-				}
-			}
-		});
+	private void initEvents() {
+		btn_yes.setOnClickListener(this);
+		btn_no.setOnClickListener(this);
+		btn_next.setOnClickListener(this);
 	}
 	
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.d_question_health_yes:
+			break;
+		case R.id.d_question_health_no:
+			
+			//moveToBack(); // 뒤로가기
+			break;
+		case R.id.imgBtn_D_Next:
+			//PromptDialog("정말 삭제하시겠습니까?");
+			break;
+		}
+	}
+	
+
+	
+/*
 	// show question view 
 	public void showQuestionView(){
 		questionText.setText(question.getQuestion());
@@ -115,6 +105,6 @@ public class QuestionActivity extends ActionBarActivity{
 		imgBtnNO.setVisibility(View.INVISIBLE);
 		imgBtnNext.setVisibility(View.VISIBLE);
 
-	}
+	} */
 	
 }
