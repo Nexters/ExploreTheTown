595cd44eb6f2218c2fc86293ac07c6defa6f90f5
Joung Daun <zxc3132@hanmail.net>
2014-08-10 02:03:18 +0900

    neighbor help complete

--- src/com/example/explorethetown/QuestionNeighborActivity.java
+++ src/com/example/explorethetown/QuestionNeighborActivity.java
@@ -14,7 +14,9 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 import android.widget.ImageView.ScaleType;
 import android.widget.SeekBar;
@@ -54,7 +56,7 @@ public class QuestionNeighborActivity extends ActionBarActivity {
 	   drawGraph(imgBgLine.getLeft(), imgBgLine.getRight(), imgBgLine.getTop(), imgBgLine.getBottom());
    }
    
-   // Go to Next
+   
    public void setOnClickListener(){
       Button imgBtnNext = (Button)findViewById(R.id.imgBtn_questionNeighbor_Next);
       
@@ -67,17 +69,39 @@ public class QuestionNeighborActivity extends ActionBarActivity {
             startActivity(iIntent);
          }
       });
+      
+      ImageButton helpBtn = (ImageButton)findViewById(R.id.imgBtn_question_neighbor_helpBtn);
+      helpBtn.setOnClickListener(new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			RelativeLayout helpLayout = (RelativeLayout)findViewById(R.id.layout_quetion_neighbor_help);
+			helpLayout.setVisibility(View.VISIBLE);
+		}
+	});
+      
+      ImageButton helpCloseBtn = (ImageButton)findViewById(R.id.imgBtn_question_neighbor_help_close);
+      helpCloseBtn.setOnClickListener(new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			RelativeLayout helpLayout = (RelativeLayout)findViewById(R.id.layout_quetion_neighbor_help);
+			helpLayout.setVisibility(View.INVISIBLE);
+		}
+	});
    }
 
    public void drawGraph(int left, int right, int top, int bottom){
-      setContentView(R.layout.activity_question_neighbor);
 
-      ViewGroup vg = (ViewGroup) findViewById(R.id.main_layout);
+
+      RelativeLayout vg = (RelativeLayout) findViewById(R.id.main_layout);
       
       
       GraphData data = new GraphData();
-      graph = new GraphView(getApplicationContext(), data, left+50 , top+30 );
-      vg.addView(graph, new LayoutParams(1000, 1300));
+      graph = new GraphView(getApplicationContext(), data, 0 , 0 );
+      vg.addView(graph, new LayoutParams(800, 700));
       // top
       seek_top = (SeekBar) findViewById(R.id.seek_question_neighbor_peopleCnt);
       seek_top.setMax(200);
@@ -137,13 +161,6 @@ public class QuestionNeighborActivity extends ActionBarActivity {
 	   imgView.getLayoutParams().width = (int) (screenWidth * 0.203703);
 	   imgView.setPadding((int) (screenWidth*0.01667), 0, 0, 0);
 	   imgView.setScaleType(ScaleType.FIT_XY);
-	   
-
-	   
-	   
-	   
-	   
-	   
 	   
 	   
 	   
